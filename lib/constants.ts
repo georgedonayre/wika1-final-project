adc77a9b63a71263a37ccf99fc87a1b7c13f76bc
dfntlygeorge <g.a.donayre.business@gmail.com>
2026-05-06 22:49:11 +0800

    chore: rename the website since bawal ang tagalog

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
-export const STORAGE_KEY = "wikaKonek_gameState";
+export const STORAGE_KEY = "kanunutan_gameState";
 export const MAX_ATTEMPTS = 5;
 export const WORDS_PER_GROUP = 4;
 export const TOTAL_GROUPS = 4;
-export const GAME_NAME = "WikaKoNek";
+export const GAME_NAME = "Kanunutan";
 export const GAME_TAGLINE = "Tagline go here";
 
 export const GROUP_THEME = {
