b034cd08659be742d7b033cd106ceaacf30a8a3e
dfntlygeorge <g.a.donayre.business@gmail.com>
2026-05-17 12:51:27 +0800

    chore: add puzzles

--- data/puzzles.ts
+++ data/puzzles.ts
@@ -7,15 +7,25 @@ import { Puzzle } from "@/types/game";
 export const puzzles: Puzzle[] = [
   {
     id: "puzzle-demo-1",
-    date: "2026-05-02",
+    date: "2026-05-17",
     groups: [
       {
         id: "g1",
         color: "yellow",
         connection: "Parte ng Katawan",
         words: [
-          { id: "w1", text: "Buntuk", language: "kapampangan", translation: "Ulo" },
-          { id: "w2", text: "Taklay", language: "pangasinense", translation: "Braso" },
+          {
+            id: "w1",
+            text: "Buntuk",
+            language: "kapampangan",
+            translation: "Ulo",
+          },
+          {
+            id: "w2",
+            text: "Taklay",
+            language: "pangasinense",
+            translation: "Braso",
+          },
           { id: "w3", text: "Talampakan ", language: "tagalog" },
           { id: "w4", text: "Balakang ", language: "tagalog" },
         ],
@@ -23,11 +33,20 @@ export const puzzles: Puzzle[] = [
       {
         id: "g2",
         color: "green",
-        connection:
-          "Mga Estruktura sa siyudad",
+        connection: "Mga Estruktura sa siyudad",
         words: [
-          { id: "w5", text: "Dalan", language: "kapampangan", translation: "Daan/Kalsada" },
-          { id: "w6", text: "Simbaan", language: "pangasinense", translation: "Simbahan" },
+          {
+            id: "w5",
+            text: "Dalan",
+            language: "kapampangan",
+            translation: "Daan/Kalsada",
+          },
+          {
+            id: "w6",
+            text: "Simbaan",
+            language: "pangasinense",
+            translation: "Simbahan",
+          },
           { id: "w7", text: "Gusali", language: "tagalog" },
           { id: "w8", text: "Tulay", language: "tagalog" },
         ],
@@ -37,11 +56,20 @@ export const puzzles: Puzzle[] = [
         color: "blue",
         connection: "Mga Financial Terms",
         words: [
-          { id: "w9", text: "Kuwalta", language: "kapampangan", translation: "Pera/Salapi" },
-          { id: "w10", text: "Manbayar", language: "pangasinense", translation: "Magbayad" },
-          { id: "w11", text: "Resibo", language: "tagalog" },
-          { id: "w12", text: "Puhunan", language: "tagalog",
+          {
+            id: "w9",
+            text: "Kuwalta",
+            language: "kapampangan",
+            translation: "Pera/Salapi",
           },
+          {
+            id: "w10",
+            text: "Manbayar",
+            language: "pangasinense",
+            translation: "Magbayad",
+          },
+          { id: "w11", text: "Resibo", language: "tagalog" },
+          { id: "w12", text: "Puhunan", language: "tagalog" },
         ],
       },
       {
@@ -49,10 +77,330 @@ export const puzzles: Puzzle[] = [
         color: "purple",
         connection: "Mga Kagamitan na Makikita sa Hardin",
         words: [
-          { id: "w13", text: "Gabu'n", language: "kapampangan", translation: "Lupa" },
-          { id: "w14", text: "Barang", language: "pangasinense", translation: "Bolo/Itak" },
+          {
+            id: "w13",
+            text: "Gabu'n",
+            language: "kapampangan",
+            translation: "Lupa",
+          },
+          {
+            id: "w14",
+            text: "Barang",
+            language: "pangasinense",
+            translation: "Bolo/Itak",
+          },
           { id: "w15", text: "Paso", language: "tagalog" },
           { id: "w16", text: "Kalaykay ", language: "tagalog" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "puzzle-demo-2",
+    date: "2026-05-18",
+    groups: [
+      {
+        id: "g1",
+        color: "yellow",
+        connection: "Parte ng Katawan",
+        words: [
+          { id: "w1", text: "Talampakan", language: "tagalog" },
+          { id: "w2", text: "Kasukasuan", language: "tagalog" },
+          { id: "w3", text: "Balakang", language: "tagalog" },
+          { id: "w4", text: "Tagiliran", language: "tagalog" },
+        ],
+      },
+      {
+        id: "g2",
+        color: "green",
+        connection: "Mga Estruktura sa Siyudad",
+        words: [
+          { id: "w5", text: "Gusali", language: "tagalog" },
+          { id: "w6", text: "Tulay", language: "tagalog" },
+          { id: "w7", text: "Dyip", language: "tagalog" },
+          { id: "w8", text: "Ordinansa", language: "tagalog" },
+        ],
+      },
+      {
+        id: "g3",
+        color: "blue",
+        connection: "Mga Pinansyal na Kataga",
+        words: [
+          { id: "w9", text: "Pitaka", language: "tagalog" },
+          { id: "w10", text: "Diskwento", language: "tagalog" },
+          { id: "w11", text: "Puhunan", language: "tagalog" },
+          { id: "w12", text: "Resibo", language: "tagalog" },
+        ],
+      },
+      {
+        id: "g4",
+        color: "purple",
+        connection: "Nakikita sa Hardin",
+        words: [
+          { id: "w13", text: "Punla", language: "tagalog" },
+          { id: "w14", text: "Asarol", language: "tagalog" },
+          { id: "w15", text: "Paso", language: "tagalog" },
+          { id: "w16", text: "Kalaykay", language: "tagalog" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "puzzle-demo-3",
+    date: "2026-05-19",
+    groups: [
+      {
+        id: "g1",
+        color: "yellow",
+        connection: "Parte ng Katawan",
+        words: [
+          {
+            id: "w1",
+            text: "Buntuk",
+            language: "kapampangan",
+            translation: "Ulo",
+          },
+          {
+            id: "w2",
+            text: "Atiyan",
+            language: "kapampangan",
+            translation: "Tiyan",
+          },
+          {
+            id: "w3",
+            text: "Bitis",
+            language: "kapampangan",
+            translation: "Paa",
+          },
+          {
+            id: "w4",
+            text: "Arung",
+            language: "kapampangan",
+            translation: "Ilong",
+          },
+        ],
+      },
+      {
+        id: "g2",
+        color: "green",
+        connection: "Mga Estruktura sa Siyudad",
+        words: [
+          {
+            id: "w5",
+            text: "Tete",
+            language: "kapampangan",
+            translation: "Tulay",
+          },
+          {
+            id: "w6",
+            text: "Bale",
+            language: "kapampangan",
+            translation: "Bahay",
+          },
+          {
+            id: "w7",
+            text: "Dalan",
+            language: "kapampangan",
+            translation: "Daan/Kalsada",
+          },
+          {
+            id: "w8",
+            text: "Gwe'bnan",
+            language: "kapampangan",
+            translation: "Gobiyerno",
+          },
+        ],
+      },
+      {
+        id: "g3",
+        color: "blue",
+        connection: "Mga Pinansyal na Kataga",
+        words: [
+          {
+            id: "w9",
+            text: "Kuwalta",
+            language: "kapampangan",
+            translation: "Pera/Salapi",
+          },
+          {
+            id: "w10",
+            text: "O'bra",
+            language: "kapampangan",
+            translation: "Trabaho",
+          },
+          {
+            id: "w11",
+            text: "Sul'it",
+            language: "kapampangan",
+            translation: "Sulit",
+          },
+          {
+            id: "w12",
+            text: "Dapa't",
+            language: "kapampangan",
+            translation: "Dapat/Kailangan",
+          },
+        ],
+      },
+      {
+        id: "g4",
+        color: "purple",
+        connection: "Nakikita sa Hardin",
+        words: [
+          {
+            id: "w13",
+            text: "Tanam",
+            language: "kapampangan",
+            translation: "Halaman/Tanim",
+          },
+          {
+            id: "w14",
+            text: "Depa'skwa",
+            language: "kapampangan",
+            translation: "Pala",
+          },
+          {
+            id: "w15",
+            text: "Gabu'n",
+            language: "kapampangan",
+            translation: "Lupa",
+          },
+          {
+            id: "w16",
+            text: "Ba'nus",
+            language: "kapampangan",
+            translation: "Walis",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "puzzle-demo-4",
+    date: "2026-05-20",
+    groups: [
+      {
+        id: "g1",
+        color: "yellow",
+        connection: "Parte ng Katawan",
+        words: [
+          {
+            id: "w1",
+            text: "Lima",
+            language: "pangasinense",
+            translation: "Kamay",
+          },
+          {
+            id: "w2",
+            text: "Sali",
+            language: "pangasinense",
+            translation: "Paa",
+          },
+          {
+            id: "w3",
+            text: "Gamet",
+            language: "pangasinense",
+            translation: "Daliri",
+          },
+          {
+            id: "w4",
+            text: "Taklay",
+            language: "pangasinense",
+            translation: "Braso",
+          },
+        ],
+      },
+      {
+        id: "g2",
+        color: "green",
+        connection: "Mga Estruktura sa Siyudad",
+        words: [
+          {
+            id: "w5",
+            text: "Simbaan",
+            language: "pangasinense",
+            translation: "Simbahan",
+          },
+          {
+            id: "w6",
+            text: "Dalan",
+            language: "pangasinense",
+            translation: "Daan/Kalsada",
+          },
+          {
+            id: "w7",
+            text: "Báley",
+            language: "pangasinense",
+            translation: "Bayan/Lungsod",
+          },
+          {
+            id: "w8",
+            text: "Abung",
+            language: "pangasinense",
+            translation: "Bahay",
+          },
+        ],
+      },
+      {
+        id: "g3",
+        color: "blue",
+        connection: "Mga Pinansyal na Kataga",
+        words: [
+          {
+            id: "w9",
+            text: "Kuwarta",
+            language: "pangasinense",
+            translation: "Pera/Salapi",
+          },
+          {
+            id: "w10",
+            text: "Mantipon",
+            language: "pangasinense",
+            translation: "Mag-ipon",
+          },
+          {
+            id: "w11",
+            text: "Manbayar",
+            language: "pangasinense",
+            translation: "Magbayad",
+          },
+          {
+            id: "w12",
+            text: "Mangaliw",
+            language: "pangasinense",
+            translation: "Mamili",
+          },
+        ],
+      },
+      {
+        id: "g4",
+        color: "purple",
+        connection: "Nakikita sa Hardin",
+        words: [
+          {
+            id: "w13",
+            text: "Dulos",
+            language: "pangasinense",
+            translation: "Asarol",
+          },
+          {
+            id: "w14",
+            text: "Katli",
+            language: "pangasinense",
+            translation: "Gunting/Pait",
+          },
+          {
+            id: "w15",
+            text: "Barang",
+            language: "pangasinense",
+            translation: "Bolo/Itak",
+          },
+          {
+            id: "w16",
+            text: "Lubir",
+            language: "pangasinense",
+            translation: "Lubid",
+          },
         ],
       },
     ],
